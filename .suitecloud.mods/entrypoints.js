5bf4f5c05fbcf936e277598826a18f4d8d649371
Octavio Quiroz <octavioquiroz30@gmail.com>
2022-09-08 14:04:26 -0500

    fix: minor fix, remove a line break in the command console.log

--- .suitecloud.mods/entrypoints.js
+++ .suitecloud.mods/entrypoints.js
@@ -4,7 +4,7 @@ const { author, version, defaultAuthId, timestamp } = require("./config");
 module.exports = {
     entryPoints: {
         beforeExecuting: (args) => {
-            console.log(colors.highlight, `> paw v${version} by ${author}\n> executed at ${timestamp}\n`);
+            console.log(colors.highlight, `\n> paw v${version} by ${author}\n> executed at ${timestamp}\n`);
             return args;
         },
         onError: (args) => args,
