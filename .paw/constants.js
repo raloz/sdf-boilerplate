a5ceca7817d9b8147ddcd3adf5b82bca82b223db
Octavio Quiroz <octavioquiroz30@gmail.com>
2022-09-08 23:04:48 -0500

    feat: user home dir path added as constant

--- .paw/constants.js
+++ .paw/constants.js
@@ -1,8 +1,11 @@
+const { homedir } = require("os");
+
 module.exports = {
     colors: {
         error: "\x1b[31m%s\x1b[0m",
         warning: "\x1b[33m%s\x1b[0m",
         info: "\x1b[36m%s\x1b[0m",
         highlight: "\x1b[35m%s\x1b[0m",
-    }
+    },
+    homedir: homedir()
 }
